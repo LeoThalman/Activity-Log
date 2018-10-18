b6b758d33170fd3c054525fc2eccb07f280fa2ea
LeoThalman <leothalman@gmail.com>
2018-10-17 21:25:44 -0700

    Changed new_activity and activities methods to save and query from mongodb

--- app.py
+++ app.py
@@ -1,27 +1,16 @@
 from flask import Flask, jsonify, abort, request, url_for
 from datetime import datetime
-import pymongo
+from pymongo import MongoClient
+
 
 app = Flask(__name__)
+client = MongoClient('mongodb://localhost:27017/')
+db = client["activity-log"]
 
-activity_log = [
-    {
-        'id': 0,
-        'user_id': 1,
-        'username': 'john',
-        'timestamp': datetime.utcnow(),
-        'details': "Important stuff here",
-    },
-    {
-        'id': 1,
-        'user_id': 2,
-        'username': 'yoko',
-        'timestamp': datetime.utcnow(),
-        'details': "Even more important",
-    },
-]
 
-@app.route("/api/activities/<int:id>", methods=["GET"])
+
+
+@app.route("/api/activities/<string:id>", methods=["GET"])
 def activity(id):
     if id < 0 or id >= len(activity_log):
         abort(404)
@@ -30,19 +19,24 @@ def activity(id):
 
 @app.route("/api/activities/", methods=["GET"])
 def activities():
-    return jsonify({"activities": activity_log})
+    act_log = db["act_log"]
+    rtn = list(act_log.find({}).sort("timestamp").limit(10))
+    for log in rtn:
+        log["_id"] = str(log["_id"])
+    return jsonify({"activities": rtn})
 
 
 @app.route("/api/activities", methods=["POST"])
 def new_activity():
+    act_log = db["act_log"]
     if not request.json:
         abort(400)
-    new_act_log= request.get_json()
+    new_act_log = request.get_json()
     if "user_id" not in new_act_log or "username" not in new_act_log or "details" not in new_act_log:
         abort(400)
-    new_act_log["id"] = len(activity_log)
     new_act_log["timestamp"] = datetime.utcnow()
-    activity_log.append(new_act_log)
+    log_id = str(act_log.insert_one(new_act_log).inserted_id)
     rtn = dict(new_act_log)
-    rtn["location"] = url_for("activity", id=new_act_log["id"])
+    rtn["_id"] = str(log_id)
+    rtn["location"] = url_for("activity", id=rtn["_id"])
     return jsonify(rtn)
